123a760a87027c628bc7c6a9c9a7bc56480c2412
478965119@qq.com <478965119@qq.com>
2019-11-07 17:43:02 +0800

    增加配置中心

---
+++ config-client/src/test/java/com/ultrapower/demo/configclient/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ultrapower.demo.configclient;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
